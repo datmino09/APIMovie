7fafa6410a8842e348b8f0b33aca92810ae21241
datmino09 <datmino93737@gmail.com>
2025-04-08 15:33:28 +0700

    changes

--- src/connectionDB.js
+++ src/connectionDB.js
@@ -15,7 +15,7 @@
 // }
 // ConnectionDatabase();
 // module.exports = sequelize;
-const { Sequelize } = require('sequelize');
+// const { Sequelize } = require('sequelize');
 
 const sequelize = new Sequelize(process.env.MYSQL_DBNAME,process.env.MYSQL_USER ,process.env.MYSQL_PASSWORD , {
     host: process.env.MYSQL_HOST,
