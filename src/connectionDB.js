dc7fad76cfbff78819d2feca0b174eddb53501b2
datmino09 <100676522+datmino09@users.noreply.github.com>
2025-04-08 15:19:06 +0700

    Update connectionDB.js

--- src/connectionDB.js
+++ src/connectionDB.js
@@ -19,6 +19,7 @@ const { Sequelize } = require('sequelize');
 
 const sequelize = new Sequelize(process.env.MYSQL_DBNAME,process.env.MYSQL_USER ,process.env.MYSQL_PASSWORD , {
     host: process.env.MYSQL_HOST,
+    port: process.env.MYSQL_PORT,
     dialect: 'mysql',
     logging: false,
 });
